e2c5a5222e97583d5ac9272834e0381de6f10816
yoneison <yoneisonbayona@gmail.com>
2024-04-29 21:30:36 -0500

    advances

--- src/models/index.js
+++ src/models/index.js
@@ -1,2 +1,9 @@
 const User = require("./User")
-const Category =require("./Category")
+const Category = require("./Category")
+const Product = require('./Product')
+
+
+//Product -> categoryId
+Product.belongsTo(Category)
+Category.hasMany(Product)
+
